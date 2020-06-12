8c62e193393c309d90939eed529341cb025126bb
BenCretois <bencretois@gmail.com>
2020-06-12 17:15:40 +0200

    Add files

---
+++ Helper_script.R
@@ -0,0 +1,11 @@
+library(xlsx)
+library(foreign)
+
+# Convert a SPSS file to an XLSX file to be read in R
+spss_to_R <- function(file, save){
+  d <- read.spss(file)
+  d <- do.call(cbind.data.frame, d)
+  d[d == -9999] <- NA
+  write.xlsx(d, save)
+}
+
